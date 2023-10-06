9bf1cb81186bb4f0a9f9c7a744dcefb5f221c79c
br0wnD3v <bothereum@protonmail.com>
2023-10-07 04:45:50 +0530

    Initial Stage

---
+++ helper/getAssetPrice.js
@@ -0,0 +1,30 @@
+const getPriceOfETH = require("./currencies/ETH.js");
+
+async function priceOfETH() {
+  return new Promise((resolve) => {
+    const value = getPriceOfETH();
+    resolve(value);
+  });
+}
+
+function loadingAnimation() {
+  const frames = ["⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏"];
+  let frameIndex = 0;
+
+  return setInterval(() => {
+    process.stdout.write("\r" + frames[frameIndex] + " ");
+
+    frameIndex++;
+    if (frameIndex === frames.length) {
+      frameIndex = 0;
+    }
+  }, 100);
+}
+
+// Start the loading animation
+const animationInterval = loadingAnimation();
+
+priceOfETH().then((value) => {
+  clearInterval(animationInterval);
+  console.log("\nETH price:", value);
+});
