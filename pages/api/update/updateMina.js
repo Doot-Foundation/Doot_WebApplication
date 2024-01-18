3ee238e2b18df603b8069e0482c9df475406c73f
br0wnD3v <bothereum@protonmail.com>
2024-01-19 01:10:14 +0530

    Runtime Increased.

--- pages/api/update/updateMina.js
+++ pages/api/update/updateMina.js
@@ -4,7 +4,7 @@ const { TOKEN_TO_CACHE, MINA_CACHE } = require("../../../utils/constants/info");
 const { redis } = require("../../../utils/helper/InitRedis");
 
 export const config = {
-  maxDuration: 60,
+  maxDuration: 120,
 };
 
 export default async function handler(req, res) {
