f94d5a8562ba2ac505a9602474e0b7670e358b87
br0wnD3v <bothereum@protonmail.com>
2024-04-01 02:32:17 +0530

    Merged individual cron calls for each token.

--- pages/api/update/updateLatestTokenSlot.js
+++ pages/api/update/updateLatestTokenSlot.js
@@ -12,54 +12,35 @@ export default async function handler(req, res) {
   const cachedData = await redis.get(TOKEN_TO_CACHE[token.toLowerCase()]);
   const toCheck = cachedData.signature;
   var originsVerified = false;
-  var isCronOperation = false;
 
-  const authHeader = req.headers.authorization;
-  if (authHeader == `Bearer ${process.env.CRON_SECRET}`) {
-    originsVerified = true;
-    isCronOperation = true;
-  } else {
-    const compatibleSignatureObject = JSON.parse(signature);
+  const compatibleSignatureObject = JSON.parse(signature);
 
-    console.log(toCheck);
+  console.log(toCheck);
 
-    var toVerify = {
-      data: toCheck.data.toString(),
-      publicKey: toCheck.publicKey.toString(),
-      signature: toCheck.signature.toString(),
-    };
-    toVerify = JSON.stringify(toVerify);
+  var toVerify = {
+    data: toCheck.data.toString(),
+    publicKey: toCheck.publicKey.toString(),
+    signature: toCheck.signature.toString(),
+  };
+  toVerify = JSON.stringify(toVerify);
 
-    const verifyBody = {
-      data: toVerify,
-      signature: compatibleSignatureObject.signature,
-      publicKey: compatibleSignatureObject.publicKey,
-    };
+  const verifyBody = {
+    data: toVerify,
+    signature: compatibleSignatureObject.signature,
+    publicKey: compatibleSignatureObject.publicKey,
+  };
 
-    console.log(verifyBody);
-    originsVerified = signatureClient.verifyMessage(verifyBody);
-  }
+  console.log(verifyBody);
+  originsVerified = signatureClient.verifyMessage(verifyBody);
 
   console.log(originsVerified);
   if (!originsVerified) res.status(201).json({ status: 0 });
-  else {
-    if (isCronOperation)
-      await appendSignatureToSlot(
-        token.toLowerCase(),
-        cachedData,
-        cachedData.signature,
-        ORACLE_PUBLIC_KEY
-      );
-    else {
-      const compatibleSignatureObject = JSON.parse(signature);
 
-      await appendSignatureToSlot(
-        token.toLowerCase(),
-        cachedData,
-        compatibleSignatureObject,
-        publicKey
-      );
-    }
-  }
+  await appendSignatureToSlot(
+    token.toLowerCase(),
+    cachedData,
+    compatibleSignatureObject,
+    publicKey
+  );
   res.status(201).json({ status: 1 });
 }
